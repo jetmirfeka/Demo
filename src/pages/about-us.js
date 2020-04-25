54f942991bfff7db96f1a89f31cff8d0cb979d88
jetmirfeka1996 <40043843+jetmirfeka1996@users.noreply.github.com>
2020-04-25 18:55:17 +0200

    test push

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -6,6 +6,7 @@ import img from '../assets/images/about-header.jpg'
 const  aboutus = () => {
     return (
         <Layout>
+            
             <MainImage imgBackground = {img} />
         </Layout>
     )
